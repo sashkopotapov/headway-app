dabad4476adb9802596a884684c2569d471c2f0e
Sashko Potapov <opotapov.business@gmail.com>
2025-02-14 00:08:15 +0200

    Initial Commit

---
+++ Headway/HeadwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadwayApp.swift
+//  Headway
+//
+//  Created by Sashko Potapov on 14.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct HeadwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HeadwayTests/HeadwayTests.swift
@@ -0,0 +1,17 @@
+//
+//  HeadwayTests.swift
+//  HeadwayTests
+//
+//  Created by Sashko Potapov on 14.02.2025.
+//
+
+import Testing
+@testable import Headway
+
+struct HeadwayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
